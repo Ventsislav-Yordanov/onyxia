faa62e549780f7c57c7f6efbf37ddc2b9f0e2414
atanassoff.i <atanassoff.i@gmail.com>
2014-07-01 00:02:23 +0300

    team responsive fixes;

--- css/onyxia.js
+++
@@ -1,6 +0,0 @@
-function toggle_div(id) {
-    var divelement = document.getElementById(id);
-    if (divelement.style.display == 'none') divelement.style.display = 'block';
-    else
-        divelement.style.display = 'none';
-}
